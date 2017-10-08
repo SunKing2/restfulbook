8f67a8cbff2740750ba879ff1317ec804318a580
SunKing2 <louievbl4t@gmail.com>
2017-10-07 20:40:19 -0400

    added commons ioutils and can now read XML from the POST request, outputs at console

--- src/mystuff/CustomerResource.java
+++ src/mystuff/CustomerResource.java
@@ -1,9 +1,12 @@
 package mystuff;
+import java.io.*;
 import java.net.URI;
 import java.util.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+
+import org.apache.commons.io.IOUtils;
 
 @Path("customers")
 public class CustomerResource {
@@ -11,10 +14,20 @@ public class CustomerResource {
 	private Map<Integer, Customer> customerDB = new HashMap<>();
 	private Integer id = 0;
 	
-	@GET
+	@POST
 	@Produces("application/xml")
-	public Response createCustomer() {
+	@Consumes("application/xml")
+	public Response createCustomer(InputStream is) {
+
+		String inputString = "";
+		try {
+			inputString = IOUtils.toString(is, "UTF-8");
+		}
+		catch (Exception exc) {
+			System.out.println("POST /customers:" + exc);
+		}
 		
+		System.out.println("POST /customers XML input:\n" + inputString);
 		Customer customer = new Customer(++id);
 		customerDB.put(id, customer);
 		
